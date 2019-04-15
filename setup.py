be03d6189886172320cafb70a9be6ad7c067c46e
Jan Okraska <jan.okraska@cern.ch>
2019-04-15 14:54:15 +0200

    installation: harmonise click and pytest versions
    * Closes reanahub/pytest-reana#41 and closes reanahub/reana#145
    
    Signed-off-by: Jan Okraska <jan.okraska@cern.ch>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ tests_require = [
     'pytest-cov>=1.8.0',
     'pytest-pep8>=1.0.6',
     'pytest-reana>=0.5.0.dev20190321',
-    'pytest>=3.8.0,<4.0.0',
+    'pytest>=3.8.0',
     'swagger_spec_validator>=2.1.0'
 ]
 
@@ -55,7 +55,7 @@ setup_requires = [
 ]
 
 install_requires = [
-    'click>=7.0,<8.0',
+    'click>=7.0',
     'Flask>=0.11',
     'fs>=2.0',
     'flask-cors>=3.0.6',
