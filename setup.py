66627471b917b23d92912411516b1626dae9eb05
Tibor Šimko <tibor.simko@cern.ch>
2020-05-25 14:06:21 +0200

    installation: bump reana-commons
    Signed-off-by: Tibor Šimko <tibor.simko@cern.ch>

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ install_requires = [
     'fs>=2.0',
     'marshmallow>2.13.0,<=2.20.1',
     'pyOpenSSL==17.5.0',
-    'reana-commons[kubernetes]>=0.6.0,<0.7.0',
+    'reana-commons[kubernetes]>=0.6.1,<0.7.0',
     'reana-db>=0.6.0,<0.7.0',
     'cookiecutter==1.6.0',
     'requests==2.20.0',
