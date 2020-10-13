30097ab6fed820a3a6fd541e8b5d9dbe0e27ec3b
Adelina Lintuluoto <adelina.eleonora.lintuluoto@cern.ch>
2020-10-13 10:32:50 +0200

    cli: add quota usage to system status
    * Closes #286.

--- reana_server/status.py
+++ reana_server/status.py
@@ -14,7 +14,18 @@ from datetime import datetime, timedelta
 from invenio_accounts.models import SessionActivity
 from reana_commons.config import SHARED_VOLUME_PATH
 from reana_db.database import Session
-from reana_db.models import Workflow, RunStatus, InteractiveSession
+from reana_db.models import (
+    Workflow,
+    RunStatus,
+    InteractiveSession,
+    User,
+    Resource,
+    UserResource,
+    ResourceType,
+    ResourceUnit,
+)
+from sqlalchemy import func
+from sqlalchemy.orm import aliased
 
 
 class REANAStatus:
@@ -251,11 +262,99 @@ class WorkflowsStatus(REANAStatus):
         }
 
 
+class QuotaUsageStatus(REANAStatus):
+    """Class to retrieve statistics related to the current REANA users quota usage."""
+
+    def __init__(self, from_=None, until=None, user=None):
+        """Initialise QuotaUsageStatus class.
+
+        :param from_: From which moment in time to collect information. Not
+            implemented yet.
+        :param until: Until which moment in time to collect information. Not
+            implemented yet.
+        :param user: A REANA-DB user model.
+        :type from_: datetime
+        :type until: datetime
+        :type user: reana_db.models.User
+        """
+        super().__init__(from_=from_, until=until, user=user)
+
+    def get_top_five_resource_usage_users(
+        self, resource_filter, order, user_resource_filter=True
+    ):
+        """Query returning top five users according to filter."""
+        user_resource_subq = (
+            Session.query(UserResource)
+            .filter(user_resource_filter)
+            .join(UserResource.resource)
+            .filter(resource_filter)
+            .subquery()
+        )
+        subq_alias = aliased(UserResource, user_resource_subq)
+        return (
+            Session.query(User, subq_alias)
+            .join(subq_alias, UserResource)
+            .group_by(User, UserResource, subq_alias)
+            .order_by(order)
+            .limit(10)
+        )
+
+    def get_top_five_all(self, resource_type):
+        """Get the top five users according to quota usage."""
+        users = self.get_top_five_resource_usage_users(
+            Resource.type_ == resource_type, func.sum(UserResource.quota_used).desc()
+        )
+        return {
+            user.email: {
+                "id": str(user.id_),
+                "used": ResourceUnit.human_readable_unit(
+                    user_resource.resource.unit, user_resource.quota_used
+                ),
+                "limit": ResourceUnit.human_readable_unit(
+                    user_resource.resource.unit, user_resource.quota_limit
+                ),
+            }
+            for user, user_resource in users
+        }
+
+    def get_top_five_limited(self, resource_type):
+        """Get the top five users with quota limit according to quota usage."""
+        users = self.get_top_five_resource_usage_users(
+            Resource.type_ == resource_type,
+            (
+                func.sum(UserResource.quota_used) / func.sum(UserResource.quota_limit)
+            ).desc(),
+            UserResource.quota_limit != 0,
+        )
+        return {
+            user.email: {
+                "id": str(user.id_),
+                "used": ResourceUnit.human_readable_unit(
+                    user_resource.resource.unit, user_resource.quota_used
+                ),
+                "limit": ResourceUnit.human_readable_unit(
+                    user_resource.resource.unit, user_resource.quota_limit
+                ),
+            }
+            for user, user_resource in users
+        }
+
+    def get_status(self):
+        """Get status summary for REANA system."""
+        return {
+            "top_five_all_disk": self.get_top_five_all(ResourceType.disk),
+            "top_five_all_cpu": self.get_top_five_all(ResourceType.cpu),
+            "top_five_limited_disk": self.get_top_five_limited(ResourceType.disk),
+            "top_five_limited_cpu": self.get_top_five_limited(ResourceType.cpu),
+        }
+
+
 STATUS_OBJECT_TYPES = {
     "interactive-sessions": InteractiveSessionsStatus,
     "workflows": WorkflowsStatus,
     "users": UsersStatus,
     "system": SystemStatus,
     "storage": StorageStatus,
+    "quota-usage": QuotaUsageStatus,
 }
 """High level REANA objects to extract information from."""
