2bd2bd497dd96c41449f31af9086ee1670bff8df
Audrius Mecionis <audrius.mecionis@gmail.com>
2020-11-19 16:36:27 +0100

    ci: pin hadolint version

--- run-tests.sh
+++ run-tests.sh
@@ -90,7 +90,7 @@ check_pytest () {
 }
 
 check_dockerfile () {
-    docker run -i --rm hadolint/hadolint < Dockerfile
+    docker run -i --rm hadolint/hadolint:v1.18.2 < Dockerfile
 }
 
 check_docker_build () {
