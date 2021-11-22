20502b7da3b790ba9a84f4ef958ccfc2930de683
Audrius Mecionis <audrius.mecionis@gmail.com>
2021-11-22 11:13:26 +0100

    installation: bump shared modules

--- setup.py
+++ setup.py
@@ -48,8 +48,8 @@ setup_requires = [
 install_requires = [
     "marshmallow>2.13.0,<=2.20.1",
     "pyOpenSSL==17.5.0",
-    "reana-commons[kubernetes,yadage]>=0.8.0a37,<0.9.0",
-    "reana-db>=0.8.0a23,<0.9.0",
+    "reana-commons[kubernetes,yadage]>=0.8.0,<0.9.0",
+    "reana-db>=0.8.0,<0.9.0",
     "requests==2.25.0",
     "rfc3987==1.3.7",
     "strict-rfc3339==0.7",
