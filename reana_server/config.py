b666bfa0edbf18bc6bf26e0dd63c82e89c76aa1f
Vladyslav Moisieienkov <vladyslav.moisieienkov@cern.ch>
2022-06-13 09:48:31 +0200

    config: fix REANA_SCHEDULER_REQUEUE_SLEEP for large numbers
    - replace int() with float() to allow "1e+06" liek numbers and "infinity"
    
    closes #502

--- reana_server/config.py
+++ reana_server/config.py
@@ -264,7 +264,7 @@ ADMIN_EMAIL = os.getenv("REANA_EMAIL_SENDER", "CHANGE_ME")
 REANA_SCHEDULER_REQUEUE_SLEEP = float(os.getenv("REANA_SCHEDULER_REQUEUE_SLEEP", "15"))
 """How many seconds to wait between consuming workflows."""
 
-REANA_SCHEDULER_REQUEUE_COUNT = int(os.getenv("REANA_SCHEDULER_REQUEUE_COUNT", "200"))
+REANA_SCHEDULER_REQUEUE_COUNT = float(os.getenv("REANA_SCHEDULER_REQUEUE_COUNT", "200"))
 """How many times to requeue workflow, in case of error or busy cluster, before failing it."""
 
 # Workflow fetcher
