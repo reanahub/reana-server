9ea6091538b5b555dc6f4f3f6a6210248960dc28
Tibor Šimko <tibor.simko@cern.ch>
2026-03-26 13:55:41 +0100

    chore(master): bump shared REANA packages as of 2026-03-26 (#764)

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ install_requires = [
     "Flask>=2.1.1,<2.3.0",  # same upper pin as invenio-base
     "gitpython>=3.1",
     "marshmallow>2.13.0,<3.0.0",
-    "reana-commons[kubernetes,yadage,snakemake,cwl]>=0.95.0a12,<0.96.0",
+    "reana-commons[kubernetes,yadage,snakemake,cwl]>=0.95.0a14,<0.96.0",
     "reana-db>=0.95.0a6,<0.96.0",
     "requests>=2.25.0",
     "tablib>=0.12.1",
